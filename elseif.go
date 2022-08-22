f3d7c2d8e01be1ce00a2b20099bc262ce705620f
laxmi1232 <103212174+laxmi1232@users.noreply.github.com>
2022-08-22 18:54:16 +0530

    Create elseif.go

---
+++ elseif.go
@@ -0,0 +1,23 @@
+package main
+
+import 
+	"fmt"
+func main(){
+   var x int =120
+  if(x == 10){
+    fmt.Printf("x is less than 20\n")
+  }else if(x == 20 ){
+    fmt.Printf("x is not less than 20")
+  }else if (x == 50){
+    fmt.Printf("laxmi")
+    
+  }else if (x == 100){
+    fmt.Printf("bhardwaj")
+  }else {
+    fmt.Printf("maa")
+  }
+  
+  
+}
+
+
