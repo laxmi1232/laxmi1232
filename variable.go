80ee436e5302821db95bf5ce70e6a88781a0ba5a
laxmi1232 <103212174+laxmi1232@users.noreply.github.com>
2022-08-22 18:29:58 +0530

    Create variable.go

---
+++ variable.go
@@ -0,0 +1,28 @@
+package main 
+import "fmt"
+
+func main(){
+var x = int64
+ x = 14
+
+fmt.Println(x)
+
+var y = bool64
+ y = true
+fmt.Println(y)
+}
+
+/*second type */
+
+package main 
+import "fmt"
+func main(){
+  
+  var x, a ,b,c = 3,4,"laxmi bhardwaj",146
+  
+  fmt.Println(x)
+  fmt.Println(a)
+  fmt.Println(a)
+  fmt.Println(c)
+  fmt.Printf("b is of type %T",b)
+}
