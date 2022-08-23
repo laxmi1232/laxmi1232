56a7307face8c645682ab662ab286c6c267dc0c2
laxmi1232 <103212174+laxmi1232@users.noreply.github.com>
2022-08-23 13:15:54 +0530

    Create forloop.go

---
+++ forloop.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main(){
+  var b int = 15
+  var c int = 0
+  
+  for a:=0; a<10; a++{
+    fmt.Printf("value of a is %d\n",a)
+  }
+  for c<b{
+  c++
+    fmt.Printf("value of c )is %d\n",c)
+  }
