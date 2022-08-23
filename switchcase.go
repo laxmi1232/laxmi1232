509d5f19f289689b5f955f210bc1d5ee25bb3e8f
laxmi1232 <103212174+laxmi1232@users.noreply.github.com>
2022-08-23 12:39:23 +0530

    Create switchcase.go

---
+++ switchcase.go
@@ -0,0 +1,25 @@
+package main
+import "fmt"
+
+func main(){
+  var g string = "A"
+  switch {
+  case g == "A":
+  fmt.Println("good grade ")
+  
+  case g == "B":
+  fmt.Println("not so good grade)
+              
+              case g == "C":
+              fmt.Println("very well grade ")
+              
+              case g == "D":
+              fmt.Println("bad grade" )
+              
+              default:
+              fmt.Println("default")
+              
+             }
+           }
+
+  
